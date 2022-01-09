5d6b1c7691b07260f9a0a1e3362b2c02ebb5ef37
Jad Rizk <jad.rizk9@gmail.com>
2022-01-09 14:00:32 +0100

    🧱 add icon component

--- libs/ui-shared/src/index.ts
+++ libs/ui-shared/src/index.ts
@@ -3,3 +3,4 @@ export * from './lib/components/GlobalStyle';
 export * from './lib/components/Typography/Typography';
 export * from './lib/components/Flex/Flex';
 export * from './lib/components/Container/Container';
+export * from './lib/components/Icon/Icon';
